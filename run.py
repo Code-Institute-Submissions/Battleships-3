bc57fc066f65c669842fd4944973084ef06a814f
MDegg <matt.degg@outlook.com>
2021-08-08 16:19:07 +0000

    create board in 5x5 format using O as board spaces

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
-# python code goes here
+"""
+Build Board
+"""
+board = []
+for i in range(0, 5):
+     board.append(['O'] * 5)
+    
+
+def print_board(board):
+    """
+    Prints board to the terminal in a 5x5 format
+    """
+    for row in board:
+        print(row)
+
+print_board(board)
