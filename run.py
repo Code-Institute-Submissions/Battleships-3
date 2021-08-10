b59bb113569a8a8c4f905e0eaf9c2268999aeab4
MDegg <matt.degg@outlook.com>
2021-08-10 18:40:16 +0000

    added loop to board game to repeat as long as the user is guessing incorrect positions, validated input to ensure chosen positions are withion the board area and raised a print statement if the chosen position has alreday been chosen previously

--- run.py
+++ run.py
@@ -20,36 +20,55 @@ def print_board(board):
 def guess_position(board):
     """
     Populates and returns random position within board, allows for
-    user input and calculates whether or not the user input is a hit or miss.
+    user input and calculates whether or not the user input is a hit or miss. 
+    Validates user input to ensure that it is within range 0,4.
     """
     computer_choice_row = int(randint(0,4))
     computer_choice_column = int(randint(0,4))
     print(computer_choice_row, computer_choice_column)
     
-    
-    guess_row = int(input('Guess Row Position: '))
-    guess_column = int(input('Guess Column Position: '))
-    computer_choice = computer_choice_column, computer_choice_row
-    user_choice = guess_column, guess_row
+    user_guesses = 0
 
-    if computer_choice == user_choice:
-        print("Hit")
+    while user_guesses <4:
+        guess_row = int(input('Guess Row Position: '))
+        guess_column = int(input('Guess Column Position: '))
+
+        computer_choice = computer_choice_column, computer_choice_row
+        user_choice = guess_column, guess_row
+
+        if computer_choice == user_choice:
+            print ("You hit the battleship!!")
+            break
+
+        elif guess_row not in range(5) or guess_column not in range(5):
+                print (f'The numbers you entered are outside of the board! You entered {guess_row} and {guess_column}. Numbers entered must be between 0 and 4')
         
-    else:
-        print('miss')
-        board[guess_row][guess_column] = 'X'
-        print_board(board)
-#def validate_input(user_choice):
-#    """
-#    Checks that input entered into the terminal is an integer between 0 and 4.
-#    """
- #   if input >= 5:
- #       print('Value entered should be a number between 0 and 4')
+        elif board[guess_column][guess_row] == 'X':
+            print('You already missed your target here!')
+            
+        else:
+            print('You missed the battleship!!')
+            board[guess_row][guess_column] = 'X'
+            print_board(board)
+            
+       
+
+
+    
+    
+    
+        
     
 
 
 
+def start_game():
+    """
+    Starts Game
+    """
+    print_board(board)
+    guess_position(board)
+    #validate_input(user_choice)
 
-print_board(board)
-guess_position(board)
-#validate_input(user_choice)
+
+start_game()
